394c85ac66067092c360a2e4d5003c3eb9ca0015
a0ki-github <aoki.hotaka@gmail.com>
2022-01-31 07:26:19 +0900

    Modify: 座標変換のメソッドを簡素化

--- src/index.js
+++ src/index.js
@@ -31,21 +31,13 @@ function caluculateWinner(squares) {
 }
 
 function makeCoordinate(nth) {
-  const n = nth + 1;
-  let col, row;
-  if (n % 3 === 0) {
-    col = 3
-  } else {
-    col = n % 3
-  }
-  if (n <= 3) {
-    row = 1
-  } else if (n <= 6) {
-    row = 2
-  } else {
-    row = 3
-  }
-  return [col, row];
+  const coordinates = [
+    [1, 1], [2, 1], [3, 1],
+    [1, 2], [2, 2], [3, 2],
+    [1, 3], [2, 3], [3, 3]
+  ]
+
+  return coordinates[nth];
 }
 
 class Board extends React.Component {
