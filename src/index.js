1d8621d14fc0221582f68a0fecf925e27ef52197
a0ki-github <aoki.hotaka@gmail.com>
2022-02-05 12:31:20 +0900

    Modify: Board でマス目を並べる部分を、ハードコーディングではなく 2 つのループを使用するように書き換え

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return(
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -51,23 +52,24 @@ class Board extends React.Component {
   }
 
   render() {
+    const rows = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ];
+    const board = rows.map((row, index) => {
+      return(
+        <div key={index} className="board-row">
+          {rows[index].map(square => {
+            return this.renderSquare(square)
+          })}
+        </div>
+      );
+    })
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
   }
